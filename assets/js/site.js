0c9adaae442036fcc25a74bf7956ba41379b48f0
Michael P <102651473+michmich16@users.noreply.github.com>
2024-06-14 08:37:31 +0200

    h

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,9 @@
 // write cool JS hwere!!
+// øvelse 1: skriv en funktion der kan hente alle produkter fra API'et
+fetchAll()
+
+function fetchAll() {
+const apiURL = "https://fakestoreapi.com/";
+
+fetch apiURL
+}
